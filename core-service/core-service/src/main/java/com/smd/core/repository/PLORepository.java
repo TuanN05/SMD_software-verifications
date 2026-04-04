208753520094e3b3af3f37f0f1b83fc4f50a43da
TuanN05 <155753537+TuanN05@users.noreply.github.com>
2026-04-04 22:59:14 +0700

    sua port 5433, them validate create plo

--- core-service/core-service/src/main/java/com/smd/core/repository/PLORepository.java
+++ core-service/core-service/src/main/java/com/smd/core/repository/PLORepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PLORepository extends JpaRepository<PLO, Long> {
     List<PLO> findByProgram_ProgramId(Long programId);
+    
+    Optional<PLO> findByPloCodeAndProgram_ProgramId(String ploCode, Long programId);
 }
